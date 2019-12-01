60846135693d1424a47ce5e3ecb16265debc312c
fl4shi3r <fl4shi3r@gmail.com>
2019-12-02 02:54:26 +0530

    Add file

--- default.py
+++ default.py
@@ -1,1 +1,47 @@
-print("Default")
+import os
+
+with open ("./defaultFiles/adultContentSites.txt", 'r') as fileobj1:
+
+    for line in fileobj1.readlines():
+        # print(line.strip())
+        with open("/etc/squid/adultContentSites.txt", 'a+')  as fileobj2:
+            fileobj2.write('*' + line.strip() + '*\n')
+        fileobj2.close()
+fileobj1.close()
+
+with open ("./defaultFiles/torServers.txt", 'r') as fileobj1:
+
+    for line in fileobj1.readlines():
+        with open("/etc/squid/torServers.txt", 'a+')  as fileobj2:
+            fileobj2.write('*' + line.strip() + '*\n')
+        fileobj2.close()
+fileobj1.close()
+
+with open ("./defaultFiles/webProxies_domainName_.txt", 'r') as fileobj1:
+
+    for line in fileobj1.readlines():
+        with open("/etc/squid/webProxies_domainName_.txt", 'a+')  as fileobj2:
+            fileobj2.write('*' + line.strip() + '*\n')
+        fileobj2.close()
+fileobj1.close()
+
+with open ("./defaultFiles/webProxies_IP_.txt", 'r') as fileobj1:
+
+    for line in fileobj1.readlines():
+        with open("/etc/squid/webProxies_IP_.txt", 'a+')  as fileobj2:
+            fileobj2.write('*' + line.strip() + '*\n')
+        fileobj2.close()
+fileobj1.close()
+
+with open ("./defaultFiles/torrentAndVpn.txt", 'r') as fileobj1:
+
+    for line in fileobj1.readlines():
+        with open("/etc/squid/torrentAndVpn.txt", 'a+')  as fileobj2:
+            fileobj2.write('*' + line.strip() + '*\n')
+        fileobj2.close()
+fileobj1.close()
+
+print("Done")
+
+print("Restarting SQUID server......")
+os.system('service squid restart')
